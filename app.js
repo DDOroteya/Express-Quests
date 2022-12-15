d7149502cab1d274d0e017c6c5be3b71babb38c0
Doroteya Donova <doroteia.donova@gmail.com>
2022-12-15 17:10:42 +0100

    Created authentification with JWT

--- app.js
+++ app.js
@@ -15,24 +15,30 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 
+// public routes
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+// private routes, authentication wall
+app.use(verifyToken); 
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
-
-const { hashPassword } = require("./auth.js");
-
-app.post("/api/users", hashPassword, userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
